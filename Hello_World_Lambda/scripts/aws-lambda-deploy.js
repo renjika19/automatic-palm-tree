18b5310ca7eeba1cc48f399ac65943ed1164f24c
renjika19 <f.opesanmi@gmail.com>
2021-10-31 11:29:51 -0400

    change lambda

--- Hello_World_Lambda/scripts/aws-lambda-deploy.js
+++ Hello_World_Lambda/scripts/aws-lambda-deploy.js
@@ -1,20 +1,9 @@
-const exec = require('child_process').exec;
-const read = require('fs').readFileSync;
 const AWS = require('aws-sdk');
 const Promise = require('bluebird');
-
-const lambdaName = process.argv[2];
-if (!lambdaName) {
-  console.error('Error: missing lambda name.');
-  process.exit(1);
-}
 
 const lambda = new AWS.Lambda({
   region: 'us-east-1'
 });
-
-const lambdaUpdateFunctionCode = Promise.promisify(lambda.updateFunctionCode.bind(lambda));
-const execCommand = Promise.promisify(exec);
 
 const cwd = process.cwd();
 const zipLambdaCommand = `
@@ -22,22 +11,11 @@ const zipLambdaCommand = `
   npm install --production &&
   zip -r ${lambdaName}.zip * --quiet`;
 
-execCommand(zipLambdaCommand)
-.then(() => {
   const lambdaUpdateFunctionCodeParams = {
     FunctionName: `${lambdaName}`,
     Publish: true,
     ZipFile: read(`${cwd}/lambda/${lambdaName}/${lambdaName}.zip`)
-  };
-  console.log('Uploading code to lambda with params:', lambdaUpdateFunctionCodeParams);
-  return lambdaUpdateFunctionCode(lambdaUpdateFunctionCodeParams);
-})
-.then(lambdaData => {
-  const lambdaVersion = lambdaData.Version;
-  console.log('Lambda code uploaded with version', lambdaVersion);
-  console.log('Deployment done');
-  return;
-})
-.catch(error => {
-  throw new Error(`Error while executing deployment script: ${error}`);
-});
+   };
+  lambdaUpdateFunctionCode(lambdaUpdateFunctionCodeParams);
+
+  
